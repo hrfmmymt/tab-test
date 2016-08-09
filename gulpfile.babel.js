30ae641100e2de9aba6b6024583d12617e661bbd
hrfmmymt <hrfmmymt@gmail.com>
2016-08-10 00:22:20 +0900

    rm comment out.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -52,16 +52,7 @@ gulp.task('babel', () => {
 gulp.task('changelog', function () {
   return gulp.src('CHANGELOG.md')
     .pipe(conventionalChangelog({
-      // conventional-changelog options go here
       preset: 'angular'
-    }, {
-      // context goes here
-    }, {
-      // git-raw-commits options go here
-    }, {
-      // conventional-commits-parser options go here
-    }, {
-      // conventional-changelog-writer options go here
     }))
     .pipe(gulp.dest('./'));
 });
