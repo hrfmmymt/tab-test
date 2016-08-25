e6ef6c59bc9b9fb088000839060d8e439d8c5f6a
hrfmmymt <hrfmmymt@gmail.com>
2016-08-25 17:21:33 +0900

    #4 arrow function.

--- docs/test/test.js
+++ docs/test/test.js
@@ -2,54 +2,57 @@
 import injectBrowser from "testium/mocha";
 import assert from "power-assert";
 
-describe("index.html", function() {
+describe("index.html", () => {
   before(injectBrowser());
+
+  let browser;
   beforeEach(function() {
-    this.browser.navigateTo("/");
+    browser = this.browser;
+    browser.navigateTo("/");
   });
 
-  it("tab1 on click", function() {
-    const tab1 = this.browser.getElement("#tab1");
+  it("tab1 on click", () => {
+    const tab1 = browser.getElement("#tab1");
     // click
     tab1.click();
     // assert
-    const tabindex = tab1.get("tabindex"),
-          section1 = this.browser.getElement("#section1").get("aria-hidden");
-    assert(tabindex >= 0);
-    assert(section1 === null);
+    const tabIndex = tab1.get("tabindex"),
+          ariaHidden = browser.getElement("#section1").get("aria-hidden");
+    assert(tabIndex >= 0);
+    assert(ariaHidden === null);
   });
 
-  it("tab2 on click", function() {
-    const tab2 = this.browser.getElement("#tab2");
+  it("tab2 on click", () => {
+    const tab2 = browser.getElement("#tab2");
     // click
     tab2.click();
     // assert
-    const tabindex = tab2.get("tabindex"),
-          section2 = this.browser.getElement("#section2").get("aria-hidden");
-    assert(tabindex >= 0);
-    assert(section2 === null);
+    const tabIndex = tab2.get("tabindex"),
+          ariaHidden = browser.getElement("#section2").get("aria-hidden");
+    assert(tabIndex >= 0);
+    assert(ariaHidden === null);
   });
 
-  it("tab3 on click", function() {
-    const tab3 = this.browser.getElement("#tab3");
+  it("tab3 on click", () => {
+    const tab3 = browser.getElement("#tab3");
     // click
     tab3.click();
     // assert
-    const tabindex = tab3.get("tabindex"),
-          section3 = this.browser.getElement("#section3").get("aria-hidden");
-    assert(tabindex >= 0);
-    assert(section3 === null);
+    const tabIndex = tab3.get("tabindex"),
+          ariaHidden = browser.getElement("#section3").get("aria-hidden");
+    assert(tabIndex >= 0);
+    assert(ariaHidden === null);
   });
 
-  it("tab4 on click", function() {
-    const tab4 = this.browser.getElement("#tab4");
+  it("tab4 on click", () => {
+    const tab4 = browser.getElement("#tab4");
     // click
     tab4.click();
     // assert
-    const tabindex = tab4.get("tabindex"),
-          section4 = this.browser.getElement("#section4").get("aria-hidden");
-    assert(tabindex >= 0);
-    assert(section4 === null);
+    const tabIndex = tab4.get("tabindex"),
+          ariaHidden = browser.getElement("#section4").get("aria-hidden");
+    assert(tabIndex >= 0);
+    assert(ariaHidden === null);
   });
 
 });
