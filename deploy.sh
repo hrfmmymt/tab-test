5f57674a84ba6289f97075b7c980076acecb2b93
hrfmmymt <hrfmmymt@gmail.com>
2016-08-29 17:36:39 +0900

    modified

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
-rm -rf public || exit 0;
-mkdir public
-
-# publicフォルダに開発用ファイルを展開する。
-cp -r src/* public
-
-cd public
 
 git config user.email "hrfmmymt@gmail.com"
 git config user.name "hrfmmymt"
 
-git init
+git checkout gh-pages
 git add .
-git commit -m "Deploy to GitHub Pages"
+git merge master
 git push --force --quiet "https://${GH_TOKEN}@github.com/hrfmmymt/tab-test.git" master:gh-pages > /dev/null 2>&1
